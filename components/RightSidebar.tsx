185a3afeab6ed05ed179446dfb3c3798fdc5bf15
Gaurav Nardia <gauravnardia07@gmail.com>
2024-06-03 22:22:36 +0530

    first commit

---
+++ components/RightSidebar.tsx
@@ -0,0 +1,12 @@
+import { cn } from '@/lib/utils'
+import React from 'react'
+
+const RightSidebar = () => {
+  return (
+    <section className={cn('right_sidebar h-[calc(100vh-5px)]',)}>
+      RightSidebar
+      </section>
+  )
+}
+
+export default RightSidebar
---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig =  {
+    providers: [
+      {
+        domain: "https://unified-bear-29.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig
